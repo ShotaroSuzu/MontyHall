f5112bab209920e3133c21b2de0bc2c54548c9e3
S.Suzuki <39001712+ShotaroSuzu@users.noreply.github.com>
2018-10-28 18:21:40 +0900

    Add: Implement creating tree doors

--- src/main/java/com/zukky/shtr/MontyHall/logic/impl/GameHostImpl.java
+++ src/main/java/com/zukky/shtr/MontyHall/logic/impl/GameHostImpl.java
@@ -2,13 +2,15 @@ package com.zukky.shtr.montyHall.logic.impl;
 
 import java.util.List;
 
+import org.assertj.core.util.Lists;
+
 import com.zukky.shtr.montyHall.entity.Door;
 import com.zukky.shtr.montyHall.logic.GameHost;
 
 public class GameHostImpl implements GameHost{
 
 	public List<Door> createDoors() {
-		return null;
+		List<Door> doors = Lists.newArrayList(new Door(), new Door(), new Door());
+		return doors;
 	}
-	
 }
